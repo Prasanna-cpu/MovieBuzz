6e690248d7843f3fd184d8098e3a74ffc23b8b4a
Prasanna Kumar <kumar22maran@gmail.com>
2024-05-21 19:01:23 +0530

    Added Header

---
+++ src/components/Header.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import MenuItem from './MenuItem'
+import { PiHouseLight } from "react-icons/pi";
+import { FcAbout } from "react-icons/fc";
+import Link from 'next/link';
+
+
+export default function Header() {
+  return (
+    <div className='flex justify-between items-center p-3 max-w-7xl mx-auto'>
+        <div className='flex gap-4'>
+            <MenuItem title="Home" address="/" Icon={PiHouseLight} />
+            <MenuItem title="About" address="/about" Icon={FcAbout} />
+        </div>
+        <Link href={'/'} className='flex gap-1 items-center'>
+            <span className='text-2xl font-bold bg-green-700 py-1 px-2 rounded-lg text-white cursor-pointer'>Movi</span>
+            <span className='text-2xl font-bold bg-orange-600 py-1 px-2 rounded-lg text-black cursor-pointer'>Buzz</span>
+        </Link>
+    </div>
+  )
+}
