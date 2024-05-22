19b09714a3c0b8c457a0152b359e03519a80436c
Prasanna Kumar <kumar22maran@gmail.com>
2024-05-22 07:02:57 +0530

    Added Content in About

---
+++ src/app/about/page.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export default function About() {
+  return (
+    <div>
+      <h1 className="text-2xl font-medium text-green-500">About</h1>
+      <p>
+        Greetings from MoviBuzz, your one-stop shop for everything movie related! With a vast database of films from many genres, eras, and 
+        locations, our website is made to satisfy the interests of movie buffs of all stripes. 
+        MoviBuzz offers a wide selection of films, including timeless classics, indie gems, and the newest blockbusters. 
+        We make an effort to offer comprehensive details about every film, such as ratings, user reviews, 
+        plot summaries, and information about the cast and crew. Your movie discovery experience will be improved by
+         our simple search and filter options, which make it simple to find exactly what you're looking for.
+      </p>
+      
+    </div>
+  );
+}
