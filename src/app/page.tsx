5409add7e7ebf813349d708d32b5db650cdb983a
Prasanna Kumar <kumar22maran@gmail.com>
2024-05-22 15:30:01 +0530

    Fetched data from remote api

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,34 @@
+import Results from '@/components/Results'
 import React from 'react'
 
-export default function Home() {
+
+const API_KEY:string|undefined=process.env.APi_KEY
+
+export default async function Home({searchParams}: Readonly<{ searchParams: { genre?: string } }>) {
+
+  const genre=searchParams.genre ?? 'fetchTrending'
+
+  const res=await fetch(
+    `https://api.themoviedb.org/3/${
+      genre==='fetchTopRated' ? '/movie/top_rated' : '/trending/all/week'
+    }?api_key=${API_KEY}&language=en-US&page=1`
+  )
+
+  const data=await res.json()
+
+  if(!res.ok){
+    throw new Error('Failed to fetch data')
+  }
+
+  // console.log(API_KEY)
+  const results=data.results;
+
+  console.log(results)
+  // 
+console.log(results)
   return (
-    <div>page</div>
+    <div>
+      <Results results={results}/>
+    </div>
   )
 }
