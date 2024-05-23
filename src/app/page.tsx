5cee0e23079162a45362136a96a4a3bf2d872880
Prasanna Kumar <kumar22maran@gmail.com>
2024-05-23 10:24:22 +0530

    Cache revalidation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,12 @@ export default async function Home({searchParams}: Readonly<{ searchParams: { ge
   const res=await fetch(
     `https://api.themoviedb.org/3/${
       genre==='fetchTopRated' ? '/movie/top_rated' : '/trending/all/week'
-    }?api_key=${API_KEY}&language=en-US&page=1`
+    }?api_key=${API_KEY}&language=en-US&page=1`,
+    {
+      next:{
+        revalidate:10000
+      }
+    }
   )
 
   const data=await res.json()
@@ -23,9 +28,9 @@ export default async function Home({searchParams}: Readonly<{ searchParams: { ge
   // console.log(API_KEY)
   const results=data.results;
 
-  console.log(results)
-  // 
-console.log(results)
+//   console.log(results)
+//   // 
+// console.log(results)
   return (
     <div>
       <Results results={results}/>
