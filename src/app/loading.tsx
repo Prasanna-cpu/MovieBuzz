29fd07d1865996ca651cc0cdccff56e392671456
Prasanna Kumar <kumar22maran@gmail.com>
2024-05-23 10:28:58 +0530

    Added loading effect

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 export default function loading() {
   return (
     <div className='flex justify-center'>
-      <Image src={'../public/spinner-animated.svg'} alt="spinner"/>
+      <Image src={'C:\\Users\\maran\\Downloads\\MoviBuzz\\movibuzz\\public\\spinner-animated.svg'} alt="spinner" width="300" height="500"/>
     </div>
   )
 }
