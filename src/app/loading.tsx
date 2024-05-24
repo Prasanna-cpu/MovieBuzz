3243bb800b449f1680ef8c9f7aabcbf7a7498eb7
Prasanna Kumar <kumar22maran@gmail.com>
2024-05-24 07:29:26 +0530

    Updated loading

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 export default function loading() {
   return (
     <div className='flex justify-center'>
-      <Image src={'C:\\Users\\maran\\Downloads\\MoviBuzz\\movibuzz\\public\\spinner-animated.svg'} alt="spinner" width="300" height="500"/>
+       <h2>Loading</h2>
     </div>
   )
 }
