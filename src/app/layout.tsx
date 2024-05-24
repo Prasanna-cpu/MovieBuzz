fe008323d0f43b9aa03202aceaf687fa978de098
Prasanna Kumar <kumar22maran@gmail.com>
2024-05-24 11:20:54 +0530

    commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import Header from "@/components/Header";
 import Providers from "./Providers";
 import NavBar from "@/components/NavBar";
 import SearchBox from "@/components/SearchBox";
+import { Suspense } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,16 +20,20 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+     
+       <html lang="en">
       <body className={inter.className}>
         <Providers>
           <Header/>
           <NavBar/>
-          <SearchBox/>
+          <Suspense>
+            <SearchBox/>
+          </Suspense>
           {children}
         </Providers>
         
       </body>
-    </html>
+      </html>
+     
   );
 }
