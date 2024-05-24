bbf3e79e716264e3a77d173efc9b65917542165f
Prasanna Kumar <kumar22maran@gmail.com>
2024-05-24 11:04:04 +0530

    commit

--- src/app/Providers.tsx
+++ src/app/Providers.tsx
@@ -4,18 +4,14 @@ import React from 'react'
 
 import {ThemeProvider} from "next-themes"
 
-export default function Providers({children}) {
+export default function Providers({ children }: { children: React.ReactNode }) {
   return (
     <div>
-        <ThemeProvider defaultTheme='system' attribute='class'>
-
-            <div className="text-gray-700 dark:text-gray-200 dark:bg-gray-700 min-h-screen">
-
-                {children}
-
-            </div>
-
-        </ThemeProvider>
+      <ThemeProvider defaultTheme="system" attribute="class">
+        <div className="text-gray-700 dark:text-gray-200 dark:bg-gray-700 min-h-screen">
+          {children}
+        </div>
+      </ThemeProvider>
     </div>
-  )
+  );
 }
--- src/components/NavBarItem.tsx
+++ src/components/NavBarItem.tsx
@@ -5,15 +5,27 @@ import React from 'react'
 
 import { useSearchParams } from 'next/navigation'
 
-export default function NavBarItem({title,param}) {
-    const searchParams=useSearchParams()
-    const genre=searchParams.get('genre')
+export default function NavBarItem({
+  title,
+  param,
+}: {
+  title: string;
+  param: string;
+}) {
+  const searchParams = useSearchParams();
+  const genre = searchParams.get("genre");
   return (
     <div>
-
-        <Link href={`/?genre=${param}`} className={`hover:text-amber-500 hover:font-bold ${genre===param ? 'underline underline-offset-8 decoration-4 decoration-amber-400 rounded-lg':''}`}>
-            {title}
-        </Link>
+      <Link
+        href={`/?genre=${param}`}
+        className={`hover:text-amber-500 hover:font-bold ${
+          genre === param
+            ? "underline underline-offset-8 decoration-4 decoration-amber-400 rounded-lg"
+            : ""
+        }`}
+      >
+        {title}
+      </Link>
     </div>
-  )
+  );
 }
