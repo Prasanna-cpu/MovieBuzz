ae48c91f6d17d1d0f9049a19346af77610c0fa71
Prasanna Kumar <kumar22maran@gmail.com>
2024-05-24 15:57:30 +0530

    commit

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,12 +1,14 @@
-import React from 'react'
+import React, { Suspense } from 'react'
 import NavBarItem from './NavBarItem'
 
 export default function NavBar() {
   return (
-    <div className="flex dark:bg-gray-500 bg-amber-100 p-5 lg:text-lg justify-center gap-4">
+    <Suspense>
+      <div className="flex dark:bg-gray-500 bg-amber-100 p-5 lg:text-lg justify-center gap-4">
         <NavBarItem title="Trending" param="fetchTrending"/>
         <NavBarItem title="Top Rated" param="fetchTopRated"/>
 
-    </div>
+      </div>
+    </Suspense>
   )
 }
